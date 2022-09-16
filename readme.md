7d2a32b2513d13b81152180bf2920625862fb87a
Nliker <tjwjdgus83@naver.com>
2022-09-16 13:16:32 +0900

    readme 추가,timeline 추가

---
+++ readme.md
@@ -0,0 +1,7 @@
+## mysql 명령어
+
+### 실행
+cd /usr/local/mysql/bin
+./mysql -u codakcodak -p 
+비밀번호 입력
+
--- sample.py
+++ sample.py
@@ -1,3 +1,3 @@
-a=set([2,3])
-a.discard(1)
-print(a)
+a=[{"user_id":3,"tweet":"hi im sam"},{"user_id":1,"tweet":"hi im david"},{"user_id":2,"tweet":"hi im john"}]
+tweetlist=[[tweet["tweet"],tweet["tweet"]]for tweet in a if tweet["user_id"]%2!=0]
+print(tweetlist)
