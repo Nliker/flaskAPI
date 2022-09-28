724770cc0ea08e4ce4d17568caab5d5cdbfe1faa
Nliker <tjwjdgus83@naver.com>
2022-09-28 13:42:59 +0900

    config test

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ db={
 JWT_SECRET_KEY="codak"
 
 DB_URL=f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
-
+test="andg"
 test_db={
     'user':'codakcodak',
     'password':'dkdrmaghEl83!',
--- test_endpoints.py
+++ test_endpoints.py
@@ -2,5 +2,10 @@ import config
 
 from sqlalchemy import create_engine,text
 
+#config.py 의 변수를 불러올땐 .을 이용한다!
+print(config.test)
+
 database=create_engine(config.test_config['DB_URL'],encoding='utf-8',max_overflow=9)
-print("성공")
+print("DB 연결성공")
+
+
